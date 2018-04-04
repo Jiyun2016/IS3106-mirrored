b48093529a5f88e223ffcbd239a8eb5dc444a460
bowen93 <hou-bowen@hotmail.com>
2018-04-04 19:37:37 +0800

    version at "admin homepage, loginpage"

--- CareTaskPro/CareTaskProMobile-war/src/java/ws/restful/ApplicationConfig.java
+++ CareTaskPro/CareTaskProMobile-war/src/java/ws/restful/ApplicationConfig.java
@@ -25,7 +25,6 @@ public class ApplicationConfig extends Application {
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(ws.restful.AdminResource.class);
-        resources.add(ws.restful.RequesterResource.class);
     }
     
 }
